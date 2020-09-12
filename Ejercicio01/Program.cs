9212b41a4840338e90b1e893fabcd96346abd3fc
FelipeBG1 <60651449+FelipeBG1@users.noreply.github.com>
2020-09-12 13:49:20 -0300

    visual

---
+++ Ejercicio01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hola");
+            Console.WriteLine("mundo");
+
+            Console.WriteLine("Ingrese el nombre: ");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(nombre);
+            Console.WriteLine(edad);
+
+            Console.WriteLine("Su nombre es {0} y su edad es {1}", nombre, edad);
+
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
